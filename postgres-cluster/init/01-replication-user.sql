2dbce501ac287219d9de2ab70a9665f6ab66b9f4
Trinh Quoc Khanh <98072456+trinhkhanh29@users.noreply.github.com>
2026-04-12 22:57:59 +0700

    upadate-Streaming-Replication

---
+++ postgres-cluster/init/01-replication-user.sql
@@ -0,0 +1,21 @@
+﻿-- Tạo replication user
+CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_pass';
+
+-- Tạo replication slot cho từng follower (giữ WAL không bị xóa khi đang clone)
+SELECT pg_create_physical_replication_slot('slot_follower_1');
+SELECT pg_create_physical_replication_slot('slot_follower_2');
+
+-- Thêm rule replication vào pg_hba.conf
+DO $$
+DECLARE
+  hba_path text;
+BEGIN
+  SELECT setting INTO hba_path FROM pg_settings WHERE name = 'hba_file';
+  EXECUTE format(
+    'COPY (VALUES (''host replication replicator 0.0.0.0/0 trust'')) TO PROGRAM %L',
+    'tee -a ' || hba_path
+  );
+  PERFORM pg_reload_conf();
+  RAISE NOTICE '[LEADER] Setup xong.';
+END;
+$$;
